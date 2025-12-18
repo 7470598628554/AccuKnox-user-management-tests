7da1a7d0f8fb9fd5f6b9536783ce0a755c7539cd
Aditya Narayan <135099526+7470598628554@users.noreply.github.com>
2025-12-18 23:21:25 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -3,11 +3,14 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   timeout: 30000,
-  retries: 0,
+
   use: {
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     screenshot: 'on',
-    video: 'retain-on-failure'
-  }
+    video: 'retain-on-failure',
+    actionTimeout: 15000
+  },
+
+  reporter: [['html', { open: 'never' }]]
 });
