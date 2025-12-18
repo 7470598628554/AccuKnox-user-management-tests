988d2097070d71bb29cdf1262f8420a6f86e2856
Aditya Narayan <135099526+7470598628554@users.noreply.github.com>
2025-12-18 23:47:24 +0530

    UserManagementPage.js

--- pages/pages/UserManagementPage.js
+++ pages/pages/UserManagementPage.js
@@ -1,17 +1,19 @@
 class UserManagementPage {
   constructor(page) {
     this.page = page;
-    this.usernameSearch =
-      page.locator('//label[text()="Username"]/../following-sibling::div/input');
+
+    this.usernameSearchInput = page.locator(
+      '//label[text()="Username"]/../following-sibling::div/input'
+    );
+
     this.searchButton = page.getByRole('button', { name: 'Search' });
-    this.noRecordsFound = page.locator('text=No Records Found');
+    this.noRecordsText = page.locator('text=No Records Found');
   }
 
   async searchUser(username) {
-    await this.usernameSearch.fill(username);
+    await this.usernameSearchInput.fill(username);
     await this.searchButton.click();
   }
 }
 
 module.exports = { UserManagementPage };
-
